897a2108df1bca799181424510b6a41eba006dd2
Vir Davinde Singh <virdavinder1996@gmail.com>
2022-06-18 23:55:48 -0400

    added longPress feature

--- app/src/main/java/com/example/finalassignmentandroid/MapsActivity.java
+++ app/src/main/java/com/example/finalassignmentandroid/MapsActivity.java
@@ -7,16 +7,14 @@ import android.app.Dialog;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.content.pm.PackageManager;
-import android.graphics.Color;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.location.Address;
 import android.location.Geocoder;
 import android.location.Location;
 import android.os.Build;
 import android.os.Bundle;
 import android.os.Looper;
-import android.text.InputType;
-import android.util.Log;
-import android.widget.EditText;
 import android.widget.Toast;
 
 import androidx.annotation.NonNull;
@@ -40,11 +38,6 @@ import com.google.android.gms.maps.model.BitmapDescriptorFactory;
 import com.google.android.gms.maps.model.LatLng;
 import com.google.android.gms.maps.model.Marker;
 import com.google.android.gms.maps.model.MarkerOptions;
-import com.google.android.gms.maps.model.Polygon;
-import com.google.android.gms.maps.model.PolygonOptions;
-import com.google.android.gms.maps.model.Polyline;
-import com.google.android.gms.maps.model.PolylineOptions;
-import com.example.finalassignmentandroid.databinding.ActivityMapsBinding;
 
 import java.io.IOException;
 import java.util.ArrayList;
@@ -54,24 +47,22 @@ import java.util.Objects;
 
 public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
 
-    private GoogleMap mMap;
-    private ActivityMapsBinding binding;
-
     private static final int REQUEST_CODE = 1;
     private static final String TAG = "MainActivity";
     private static final long UPDATE_INTERVAL = 5000;
     private static final long FASTEST_INTERVAL = 3000;
-    int mName = 0;
-
-
+    private final List<Marker> markerList = new ArrayList<>();
+    SQLiteDatabase sqLiteDatabase;
+    int id;
+    private GoogleMap mMap;
+    private ActivityMapsBinding binding;
     private FusedLocationProviderClient mClient;
     private LocationRequest locationRequest;
     private LocationCallback locationCallback;
-    private final List<Marker> markerList = new ArrayList<>();
-    private List<MarkerModel> markerModelList = new ArrayList<>();
-    private MarkerModel markerModel ;
+    private final List<MarkerModel> markerModelList = new ArrayList<>();
+    private MarkerModel markerModel;
 
-    private Marker userMarker, favMarker , visitedMarker ;
+    private Marker userMarker, favMarker, visitedMarker;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -80,14 +71,16 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         binding = ActivityMapsBinding.inflate(getLayoutInflater());
         setContentView(binding.getRoot());
 
+        sqLiteDatabase = openOrCreateDatabase("products_db", MODE_PRIVATE, null);
+
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
         SupportMapFragment mapFragment = (SupportMapFragment) getSupportFragmentManager()
                 .findFragmentById(R.id.map);
         mapFragment.getMapAsync(this);
 
         mClient = LocationServices.getFusedLocationProviderClient(this);
+        //createTables();
     }
-
 
 
     @Override
@@ -107,94 +100,146 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         }
 
 
-
-
-
         Intent i = getIntent();
-        Double lat = i.getDoubleExtra("lat",0);
-        Double lng = i.getDoubleExtra("lng",0);
-        System.out.println("lat : " + lat +"long : " + lng);
-        setMarker( new LatLng(lat,lng));
+        Double lat = i.getDoubleExtra("lat", 0);
+        Double lng = i.getDoubleExtra("lng", 0);
+        id = i.getIntExtra("id", 0);
+        System.out.println("lat : " + lat + "long : " + lng + "----" + id);
+        setMarker(new LatLng(lat, lng));
 
 
-
-
-
-        mMap.setOnMapClickListener(new GoogleMap.OnMapClickListener() {
+        mMap.setOnMapLongClickListener(new GoogleMap.OnMapLongClickListener() {
             @Override
-            public void onMapClick(@NonNull LatLng latLng) {
-
-
+            public void onMapLongClick(@NonNull LatLng latLng) {
                 setMarker(latLng);
-               String address =  getAddress(latLng);
-               String city = getCity(latLng);
-                MarkerModel markerModel = new MarkerModel(0, latLng.latitude, latLng.longitude, city , address , 0);
-                // markerModelList.add(markerModel);
+                String address = getAddress(latLng);
+                String city = getCity(latLng);
+                MarkerModel markerModel = new MarkerModel(0, city, address, latLng.latitude, latLng.longitude, 0);
+                markerModelList.add(markerModel);
 
 
-
-
+                String sql1 = "INSERT INTO markers( city, address  , latitude ,longitude , isvisited )" +
+                        "VALUES (?,?,?,?,?)";
+                sqLiteDatabase.execSQL(sql1, new String[]{city, address, String.valueOf(latLng.latitude), String.valueOf(latLng.longitude), "0"});
 
             }
-
-
         });
-
-
-        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+        mMap.setOnMarkerDragListener(new GoogleMap.OnMarkerDragListener() {
             @Override
-            public boolean onMarkerClick(Marker markers) {
-                Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
-                List<Address> addresses = null; //1 num of possible location returned
-                String address ,city ,country , postalCode ;
-                try {
-                    addresses = geocoder.getFromLocation(markers.getPosition().latitude, markers.getPosition().longitude, 1);
-                } catch (IOException e) {
-                    e.printStackTrace();
-                }
+            public void onMarkerDrag(@NonNull Marker marker) {
 
-                if(Objects.requireNonNull(addresses).isEmpty())
-                {
-                    address = "date";
-                    city = country = postalCode = "";
-                }
-                else {
+            }
 
-                    address = addresses.get(0).getAddressLine(0); //0 to obtain first possible address
-                     city = addresses.get(0).getLocality();
-                    String state = addresses.get(0).getAdminArea();
-                     country = addresses.get(0).getCountryName();
-                     postalCode = addresses.get(0).getPostalCode();
+            @Override
+            public void onMarkerDragEnd(@NonNull Marker marker) {
+                String sql = "UPDATE markers SET latitude = ? , longitude = ?  WHERE id = ? ";
+                sqLiteDatabase.execSQL(sql,
+                        new String[]{
+                                String.valueOf(marker.getPosition().latitude),
+                                String.valueOf(marker.getPosition().longitude),
+                                String.valueOf(id)
+                        });
 
-                }
-                //create your custom title
-                String title = address ;
-                markers.setTitle(title);
-               markers.setSnippet(postalCode + " , " + city + " , " + country);
-                markers.showInfoWindow();
-                return true;
+//                            loadMarkers();
+            }
+
+
+            @Override
+            public void onMarkerDragStart(@NonNull Marker marker) {
+
             }
         });
+
+
+//        mMap.setOnMarkerClickListener(new GoogleMap.OnMarkerClickListener() {
+//            @Override
+//            public boolean onMarkerClick(Marker markers) {
+//                Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+//                List<Address> addresses = null; //1 num of possible location returned
+//                String address, city, country, postalCode;
+//                try {
+//                    addresses = geocoder.getFromLocation(markers.getPosition().latitude, markers.getPosition().longitude, 1);
+//                } catch (IOException e) {
+//                    e.printStackTrace();
+//                }
+//
+//                if (Objects.requireNonNull(addresses).isEmpty()) {
+//                    address = "date";
+//                    city = country = postalCode = "";
+//                } else {
+//
+//                    address = addresses.get(0).getAddressLine(0); //0 to obtain first possible address
+//                    city = addresses.get(0).getLocality();
+//                    String state = addresses.get(0).getAdminArea();
+//                    country = addresses.get(0).getCountryName();
+//                    postalCode = addresses.get(0).getPostalCode();
+//
+//                }
+//                //create your custom title
+//                String title = address;
+//                markers.setTitle(title);
+//                markers.setSnippet(postalCode + " , " + city + " , " + country);
+//                markers.showInfoWindow();
+//                return true;
+//            }
+//        });
+    }
+
+    //    private void createTables() {
+//
+//        String sql = "CREATE TABLE IF NOT EXISTS markers (" +
+//                "id INTEGER NOT NULL CONSTRAINT markers_pk PRIMARY KEY AUTOINCREMENT , " +
+//                "city VARCHAR(20) NOT NULL ,  " +
+//                "address VARCHAR(20) NOT NULL ,  " +
+//                "latitude DOUBLE NOT NULL , " +
+//                "longitude DOUBLE NOT NULL , " +
+//                "isVisited INTEGER NOT NULL );";
+//
+//        sqLiteDatabase.execSQL(sql);
+//    }
+    private void loadMarkers() {
+
+        String sql = "SELECT *  FROM markers";
+        Cursor cursor = sqLiteDatabase.rawQuery(sql, null);
+        markerModelList.clear();
+
+        if (cursor.moveToFirst()) {
+            do {
+                // fetch info from columns
+                markerModelList.add(
+                        new MarkerModel(
+                                cursor.getInt(0),
+                                cursor.getString(1),
+                                cursor.getString(2),
+                                cursor.getDouble(3),
+                                cursor.getDouble(4),
+                                cursor.getInt(5)
+
+                        )
+                );
+
+            } while (cursor.moveToNext());
+            cursor.close();
+        }
+
+
     }
 
     private String getCity(LatLng latLng) {
 
         Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
         List<Address> addresses = null; //1 num of possible location returned
-        String address ,city ,country , postalCode ;
+        String address, city, country, postalCode;
         try {
             addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if(Objects.requireNonNull(addresses).isEmpty())
-        {
+        if (Objects.requireNonNull(addresses).isEmpty()) {
             address = "date";
             city = country = postalCode = "";
-        }
-        else {
-
+        } else {
 
 
             address = addresses.get(0).getAddressLine(0); //0 to obtain first possible address
@@ -208,64 +253,84 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
 
         }
 
-        return  city;
+        return city;
     }
 
 
-    private String getAddress(LatLng latlng)
-    {
+    private String getAddress(LatLng latlng) {
         Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
         List<Address> addresses = null; //1 num of possible location returned
-        String address ,city ,country , postalCode ;
+        String address, city, country, postalCode;
         try {
             addresses = geocoder.getFromLocation(latlng.latitude, latlng.longitude, 1);
         } catch (IOException e) {
             e.printStackTrace();
         }
 
-        if(Objects.requireNonNull(addresses).isEmpty())
-        {
+        if (Objects.requireNonNull(addresses).isEmpty()) {
             address = "date";
             city = country = postalCode = "";
-        }
-        else {
-
+        } else {
 
 
             address = addresses.get(0).getAddressLine(0); //0 to obtain first possible address
             city = addresses.get(0).getLocality();
 
             //MarkerModel markerModel = new MarkerModel(0, latlng.latitude, latlng.longitude, city , false);
-           // markerModelList.add(markerModel);
+            // markerModelList.add(markerModel);
 //            String state = addresses.get(0).getAdminArea();
 //            country = addresses.get(0).getCountryName();
 //            postalCode = addresses.get(0).getPostalCode();
 
         }
 
-        return  address;
+        return address;
 
     }
+
     private void setMarker(LatLng latLng) {
 
-        String ch = "A";
 
 
         MarkerOptions options = new MarkerOptions()
-                .position(latLng).title(ch)
-                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE))
-                .snippet("nice place");
-                 options.draggable(true);
+                .position(latLng)
+                .icon(BitmapDescriptorFactory.defaultMarker(BitmapDescriptorFactory.HUE_AZURE));
+        options.draggable(true);
         favMarker = mMap.addMarker(options);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng , 13));
-      //  mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(latLng, 13));
+        //  mMap.moveCamera(CameraUpdateFactory.newLatLng(latLng));
+        Geocoder geocoder = new Geocoder(MapsActivity.this, Locale.getDefault());
+        List<Address> addresses = null; //1 num of possible location returned
+        String address, city, country, postalCode;
+        try {
+            addresses = geocoder.getFromLocation(latLng.latitude, latLng.longitude, 1);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
 
-        favMarker.setTag(mName);
-        mName++;
-        markerList.add(favMarker);
+        if (Objects.requireNonNull(addresses).isEmpty()) {
+            address = "date";
+            city = country = postalCode = "";
+        } else {
+
+            address = addresses.get(0).getAddressLine(0); //0 to obtain first possible address
+            city = addresses.get(0).getLocality();
+            String state = addresses.get(0).getAdminArea();
+            country = addresses.get(0).getCountryName();
+            postalCode = addresses.get(0).getPostalCode();
+
+        }
+        //create your custom title
+        String title = address;
+        favMarker.setTitle(title);
+        favMarker.setSnippet(postalCode + " , " + city + " , " + country);
+        favMarker.showInfoWindow();
+
 
 
     }
+
+
 
     @SuppressLint("MissingPermission")
     private void startUpdatingLocation() {
@@ -282,8 +347,8 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
                 Location location = locationResult.getLastLocation();
                 LatLng userLocation = new LatLng(location.getLatitude(), location.getLongitude());
 
-           //userMarker = mMap.addMarker(new MarkerOptions().position(userLocation).title("Vir").snippet("your are here"));
-              //  mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
+                //userMarker = mMap.addMarker(new MarkerOptions().position(userLocation).title("Vir").snippet("your are here"));
+                //  mMap.moveCamera(CameraUpdateFactory.newLatLng(userLocation));
             }
         };
 
@@ -299,6 +364,7 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
         return ActivityCompat.checkSelfPermission(this,
                 Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED;
     }
+
     @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
